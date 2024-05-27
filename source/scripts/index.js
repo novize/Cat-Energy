80cca3c4bc1a1854cdfa029bef2568246455e303
Сергей <zamery14@yandex.ru>
2024-05-27 20:56:04 +0300

    Module9-task1 (#23)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,17 @@
 /* в этот файл добавляет скрипты*/
+
+const navToggle = document.querySelector('.nav-control');
+const navMain = document.querySelector('.navigation__list');
+
+navToggle.addEventListener('click', () => {
+  if (navToggle.classList.contains('nav-control--close') && navMain.classList.contains('navigation__list--hidden')) {
+    navToggle.classList.remove('nav-control--close');
+    navToggle.classList.add('nav-control--open');
+    navMain.classList.remove('navigation__list--hidden');
+
+  } else {
+    navToggle.classList.remove('nav-control--open');
+    navToggle.classList.add('nav-control--close');
+    navMain.classList.add('navigation__list--hidden');
+  }
+});
