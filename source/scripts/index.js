afaa8a3ac36c60527a75c24fa041c8602abeafd3
Сергей Резник <zamery14@yandex.ru>
2024-06-02 16:12:44 +0300

    #1-2

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,6 +3,17 @@
 const navToggle = document.querySelector('.nav-control');
 const navMain = document.querySelector('.navigation__list');
 
+const statHeader = document.querySelector('.header--no-js');
+const statTogle = document.querySelector('.nav-control--no-js');
+const statNav = document.querySelector('.navigation--no-js');
+const statNavlist = document.querySelector('.navigation__list--no-js');
+
+statHeader.classList.remove('header--no-js');
+statTogle.classList.remove('nav-control--no-js');
+statNav.classList.remove('navigation--no-js');
+statNavlist.classList.remove('navigation__list--no-js');
+statNavlist.classList.add('navigation__list--hidden');
+
 navToggle.addEventListener('click', () => {
   if (navToggle.classList.contains('nav-control--close') && navMain.classList.contains('navigation__list--hidden')) {
     navToggle.classList.remove('nav-control--close');
